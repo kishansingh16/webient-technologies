667f0e791bcf88e3a4b89c516706144fa8b52bae
kishansingh16 <singhkishan1606@gmail.com>
2023-02-08 17:14:26 +0530

    commit:

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,64 +3,51 @@ import React, { useState, useEffect } from "react";
 let photo=require("../Images/golden.png");
 
 function Body(){
-    const boxImage=[
-        {count: 1},
-        {count: 2},
-        {count: 3} ,
-        {count: 4},
-        {count: 5},
-        {count: 6},
-        {count: 7} ,
-	    {count: 8},
-        {count: 9},
-        {count: 10},
-        {count: 11} ,
-	    {count: 12}  
-    ]
+   const ImageData = [
+    { count: 1 },
+    { count: 2 },
+    { count: 3 },
+    { count: 4 },
+    { count: 5 },
+    { count: 6 },
+    { count: 7 },
+    { count: 8 },
+    { count: 9 },
+    { count: 10},
+    { count: 11},
+    { count:12 },
+  ];
 
-    useEffect(() => {
-        let index = 0;
-        const interval = setInterval(() => {
-        const currentImages = boxImage;
-        console.log(currentImages);
-        if(index === 0){
-            currentImages[index].showImage = true
-            index +=1
-        }else{
-            currentImages[index+1].showImage = true
-            currentImages[index-1].showImage = false
-            index +=1;
-        }
-           setboxImages(currentImages)        
-        }, 1000);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-        return () => clearInterval(interval);
-}, []);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (currentIndex < 12) {
+        setCurrentIndex(currentIndex + 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex]);
 
     return(
-        <>
-           <div className="outer-box">
-                    <div className="container">
-                        {
-                    boxImage.map((item) => <div>
-                   {item.showImage ? item.image : item.count}
-</div>)}
-                        {/* <div className="item">1</div>
-                        <div className="item">2</div>
-                        <div className="item">3</div>
-                        <div className="item">4</div>
-                        <div className="item">5</div>
-                        <div className="item">6</div>
-                        <div className="item">7</div>
-                        <div className="item">8</div>
-                        <div className="item">9</div>
-                        <div className="item">10</div>
-                        <div className="item">11</div>
-                        <div className="item">12</div>    */}
-                    </div>
-                
-           </div>
-        </>
-    )
+   <div className="container">
+    <div className="inner-container">
+      {ImageData.map((item, index) => {
+        return (
+          <div>
+            {index !== 0 && index === currentIndex ? (
+              <h1>{item.count}</h1>
+            ) : (
+              <div className="photo">
+                <img src={"photo"} alt="BigCo" />
+              </div>
+            )}
+          </div>
+        );
+      })}
+      </div>
+    </div>
+  );
 }
 export default Body;
